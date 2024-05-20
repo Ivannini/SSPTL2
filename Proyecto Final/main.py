3a441ca834e7929082506adc967994e50b8b2f55
Nudelstejer Gomez Ivan <99306363+Ivannini@users.noreply.github.com>
2024-05-19 22:58:53 -0600

    Add files via upload

---
+++ Proyecto Final/main.py
@@ -0,0 +1,15 @@
+
+from io import open
+import elementoPila
+from analisisLexico2 import analizador
+from pila import stack
+from analisisSintactico import sintactico
+from analisisLexico import *
+
+analizador = sintactico()
+file = open("test.txt")
+test = file.read()
+print("\n----------CÓDIGO ----------\n\n", test)
+lexicoAux = AnalisisLexico(test)
+lexicoAux = lexicoAux.rstrip(lexicoAux[-1])
+analizador.compilador(lexicoAux)
